ef4b3c31972c87d8a83380994160f56e50c390ee
Jepzaru <jefffrancis.conson@cit.edu>
2024-06-05 15:14:30 +0800

    update

--- portfolio/my-portfolio/vite.config.js
+++ portfolio/my-portfolio/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/JepzFolio/portfolio/my-portfolio/', // Adjust this to match your subdirectory
+  plugins: [react()]
+});
